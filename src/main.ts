04d1b533c53972ae51eef097082c054ffaa73d80
Michael Filbin <michael.filbin@gmail.com>
2026-07-14 08:20:59 -0600

    feat: wire RibbonManager and settings into the plugin entry point

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,51 @@
-import { Plugin } from "obsidian";
+import { MarkdownView, Plugin } from "obsidian";
+import { DEFAULT_SETTINGS, type RibbonBarSettings } from "./settings";
+import { RibbonBarSettingTab } from "./settings-tab";
+import type { RibbonBarPluginLike } from "./plugin-contract";
+import { RibbonManager } from "./ribbon/RibbonManager";
 
-export default class RibbonBarPlugin extends Plugin {
-  async onload(): Promise<void> {}
+export default class RibbonBarPlugin extends Plugin implements RibbonBarPluginLike {
+  settings: RibbonBarSettings = DEFAULT_SETTINGS;
+  ribbonManager!: RibbonManager;
 
-  onunload(): void {}
+  async onload(): Promise<void> {
+    await this.loadSettings();
+
+    this.ribbonManager = new RibbonManager({
+      enabled: this.settings.ribbonEnabled,
+      defaultCollapsed: this.settings.defaultCollapsed,
+    });
+
+    this.addSettingTab(new RibbonBarSettingTab(this.app, this));
+
+    this.app.workspace.onLayoutReady(() => this.syncRibbons());
+    this.registerEvent(this.app.workspace.on("active-leaf-change", () => this.syncRibbons()));
+    this.registerEvent(this.app.workspace.on("layout-change", () => this.syncRibbons()));
+    this.registerEvent(this.app.workspace.on("file-open", () => this.syncRibbons()));
+  }
+
+  onunload(): void {
+    this.ribbonManager.detachAll(this.markdownViews());
+  }
+
+  syncRibbons(): void {
+    this.ribbonManager.syncAllLeaves(this.markdownViews());
+  }
+
+  setRibbonEnabled(enabled: boolean): void {
+    this.ribbonManager.setEnabled(enabled);
+    this.syncRibbons();
+  }
+
+  private markdownViews(): MarkdownView[] {
+    return this.app.workspace.getLeavesOfType("markdown").map((leaf) => leaf.view as MarkdownView);
+  }
+
+  async loadSettings(): Promise<void> {
+    this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+  }
+
+  async saveSettings(): Promise<void> {
+    await this.saveData(this.settings);
+  }
 }
